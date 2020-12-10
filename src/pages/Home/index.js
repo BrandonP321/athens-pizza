a0c817f2a2e51741c643f91426dfff9aa7260bf9
Brandon Phillips <brandon.phillips976@gmail.com>
2020-12-09 22:19:59 -0800

    added skeleton of location section of home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../../components/Hero'
+import LocationSection from '../../components/LocationSection'
 
 export default function Home() {
     return (
         <div>
             <Hero />
+            <LocationSection />
         </div>
     )
 }
