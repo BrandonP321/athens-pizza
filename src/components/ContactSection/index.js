0c1e2133e51cd8d67878281e5e81e869d747ce77
Brandon Phillips <brandon.phillips976@gmail.com>
2020-12-10 18:27:40 -0800

    added mobile responsive social media buttons

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -11,6 +11,13 @@ export default function ContactSection() {
             <div className='contact-text-container'>
                 <h2>Contact Us</h2>
                 <p>This is some text about contacting us!</p>
+                <div className='socials-flex'>
+                    <a href='https://www.facebook.com/Athenspizzaauburn' target='_blank' className='social-link'>
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href='https://www.instagram.com/athenspizzaauburn/' target='_blank' className='social-link'><i class="fab fa-instagram"></i></a>
+                    <a href='#' className='social-link'><i class="fab fa-twitter"></i></a>
+                </div>
             </div>
             <div className='contact-form-container'>
                 <form className='contact-form'>
@@ -30,7 +37,16 @@ export default function ContactSection() {
                         <label for='message-input'>Message</label>
                         <textarea className='form-control' id='message-input'></textarea>
                     </div>
-                    <button className='contact-submit-btn' onClick={handleFormSubmit}>Send</button>
+                    <div className='contact-form-btns'>
+                        <button className='contact-submit-btn' onClick={handleFormSubmit}>Send</button>
+                        <div className='mobile-socials'>
+                            <a href='https://www.facebook.com/Athenspizzaauburn' target='_blank' className='social-link'>
+                                <i class="fab fa-facebook"></i>
+                            </a>
+                            <a href='https://www.instagram.com/athenspizzaauburn/' target='_blank' className='social-link'><i class="fab fa-instagram"></i></a>
+                            <a href='#' className='social-link'><i class="fab fa-twitter"></i></a>
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
