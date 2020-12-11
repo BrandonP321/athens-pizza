1387ab1f2ed6cb3265c2bca0cca7b2d048e79024
Brandon Phillips <brandon.phillips976@gmail.com>
2020-12-10 20:28:01 -0800

    contact info is now collected to be sent in an email

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 
 export default function ContactSection() {
+    const [contactInfo, setContactInfo] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+    })
+
+    const handleInputChange = event => {
+        // grab name and value of property being changed
+        const { name, value } = event.target
+        // udpate state with new value
+        setContactInfo({
+            ... contactInfo,
+            [name]: value
+        })
+    }
+
     const handleFormSubmit = event => {
         event.preventDefault();
+        console.log(contactInfo)
     }
 
     return (
@@ -20,25 +38,25 @@ export default function ContactSection() {
                 </div>
             </div>
             <div className='contact-form-container'>
-                <form className='contact-form'>
+                <form className='contact-form' onSubmit={handleFormSubmit}>
                     <div className='form-group name-form-group'>
-                        <label for='name-input'>Name</label>
-                        <input className='form-control' id='name-input' type='text' />
+                        <label for='name-input'><strong>Name</strong></label>
+                        <input className='form-control' id='name-input' type='text' name='name' value={contactInfo.name} onChange={handleInputChange} />
                     </div>
                     <div className='form-group email-form-group'>
-                        <label for='email-input'>Email</label>
-                        <input className='form-control' id='email-input' type='email' />
+                        <label for='email-input'><strong>Email</strong></label>
+                        <input className='form-control' id='email-input' type='email' name='email' value={contactInfo.email} onChange={handleInputChange} />
                     </div>
                     <div className='form-group'>
-                        <label for='subject-input'>Subject</label>
-                        <input className='form-control' id='subject-input' type='text' />
+                        <label for='subject-input'><strong>Subject</strong></label>
+                        <input className='form-control' id='subject-input' type='text' name='subject' value={contactInfo.subject} onChange={handleInputChange} />
                     </div>
                     <div className='form-group'>
-                        <label for='message-input'>Message</label>
-                        <textarea className='form-control' id='message-input'></textarea>
+                        <label for='message-input'><strong>Message</strong></label>
+                        <textarea className='form-control' id='message-input' name='message' onChange={handleInputChange} >{contactInfo.message}</textarea>
                     </div>
                     <div className='contact-form-btns'>
-                        <button className='contact-submit-btn' onClick={handleFormSubmit}>Send</button>
+                        <button className='contact-submit-btn'>Send</button>
                         <div className='mobile-socials'>
                             <a href='https://www.facebook.com/Athenspizzaauburn' target='_blank' className='social-link'>
                                 <i class="fab fa-facebook"></i>
