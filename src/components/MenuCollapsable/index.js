914a07f0df80365ec1f06e4b3120e3e2c047743b
Brandon Phillips <brandon.phillips976@gmail.com>
2020-12-11 21:32:29 -0800

    added mobile responsiveness to menu

--- src/components/MenuCollapsable/index.js
+++ src/components/MenuCollapsable/index.js
@@ -20,13 +20,26 @@ export default function MenuCollapsable(props) {
                 <div className='menu-header-img-dark-overlay'></div>
                 <div className='menu-header-overlay'>
                     <div className='menu-header-text'>
-                        <h2>{props.group}</h2>
+                        <h2>{props.group}
+                        <span className='menu-dropdown-icon-container'><i className="menu-dropdown-icon fas fa-plus"></i></span>
+                        </h2>
                     </div>
                 </div>
             </div>
             <Collapse in={open}>
                 <div className='menu-collapse-body'>
-                    <h1>Open</h1>
+                    <h2>{props.group}</h2>
+                    <p className='menu-group-desc'>This is a description of this food group</p>
+                    <div className='menu-food-flex'>
+                        {props.food.map(foodItem => {
+                            return (
+                                <div className='menu-food-item'>
+                                    <h3 className='food-name'>{foodItem.name}</h3>
+                                    <p className='food-desc'>{foodItem.description}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
             </Collapse>
         </div>
