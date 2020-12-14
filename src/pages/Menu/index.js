05c6509734d789045f8fd4a0dcc506324ec7df45
Brandon Phillips <brandon.phillips976@gmail.com>
2020-12-14 13:08:14 -0800

    added hero image to menu page

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -6,7 +6,15 @@ import './index.css'
 export default function Menu() {
     return (
         <>
-            <h1>Menu</h1>
+            <div className='menu-hero-container'>
+                <img className='menu-hero-img' src='assets/images/menu-hero.jpg' />
+                <div className='menu-hero-overlay'>
+                    <div className='menu-hero-text'>
+                        <h1>Our Menu</h1>
+                        <p>this is some text about our menu</p>
+                    </div>
+                </div>
+            </div>
             {allFood.map(foodGroup => <MenuCollapsable group={foodGroup.group} img={foodGroup.image} food={foodGroup.food} />)}
         </>
     )
