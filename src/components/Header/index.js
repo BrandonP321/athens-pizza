a96266802711665c7999b57a9d7379989763760f
Brandon Phillips <brandon.phillips976@gmail.com>
2020-12-15 13:48:39 -0800

    header now changes as user scrolls down

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './index.css'
 
 export default function Header() {
+    const [isAtTop, setIsAtTop] = useState(true)
+
+    // call scroll handler when user scrolls
+    window.onscroll = function () { handleScroll() }
+
+    // update header background when user scrolls
+    const handleScroll = () => {
+        if (isAtTop && window.pageYOffset > 100) {
+            setIsAtTop(false)
+        } else if (!isAtTop && window.pageYOffset <= 100) {
+            setIsAtTop(true)
+        }
+    }
+
     return (
-        <div>
-            
-        </div>
+        <header>
+            <div className={isAtTop ? 'header-bg': 'header-bg active-header'} style={!isAtTop ? { height: '100%' }: {}}></div>
+            <div className='header-flex' style={!isAtTop? {color: 'black'}: {}}>
+                <a className='header-logo' href='/'>Athens</a>
+                <div className='nav-flex' >
+                    <a className='nav-item' href='/menu'>Menu</a>
+                    <a className='nav-item' href='/#about'>About Us</a>
+                    <a className='nav-item' href='/#location'>Location</a>
+                    <a className='nav-item' href='/#contact'>Contact</a>
+                </div>
+            </div>
+        </header>
     )
 }
