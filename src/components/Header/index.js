c5fae0f48debf5c0f8f0ed861d2dd6d24dc507ed
Brandon Phillips <brandon.phillips976@gmail.com>
2020-12-15 17:35:53 -0800

    made header responsive with separate animated mobile menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react'
 import './index.css'
 
 export default function Header() {
+    // state indicates whether user is scrolled to top of page
     const [isAtTop, setIsAtTop] = useState(true)
+
+    // state indicates whether mobile menu is displayed or not
+    const [showMobileMenu, setShowMobileMenu] = useState(false)
 
     // call scroll handler when user scrolls
     window.onscroll = function () { handleScroll() }
@@ -16,17 +20,35 @@ export default function Header() {
         }
     }
 
+    // show menu when user clicks menu icon on mobile
+    const handleMobileMenuToggle = () => {
+        console.log('mobile clicked')
+        setShowMobileMenu(!showMobileMenu)
+    }
+
     return (
         <header>
-            <div className={isAtTop ? 'header-bg': 'header-bg active-header'} style={!isAtTop ? { height: '100%' }: {}}></div>
-            <div className='header-flex' style={!isAtTop? {color: 'black'}: {}}>
+            <div className={isAtTop ? 'header-bg' : 'header-bg active-header'} style={!isAtTop ? { height: '100%' } : {}}></div>
+            <div className='header-flex' style={!isAtTop ? { color: 'black' } : {}}>
                 <a className='header-logo' href='/'>Athens</a>
                 <div className='nav-flex' >
                     <a className='nav-item' href='/menu'>Menu</a>
                     <a className='nav-item' href='/#about'>About Us</a>
                     <a className='nav-item' href='/#location'>Location</a>
                     <a className='nav-item' href='/#contact'>Contact</a>
+                    <button className='icon-btn-container' onClick={handleMobileMenuToggle}>
+                        <i className="mobile-menu-icon fas fa-bars"></i>
+                    </button>
                 </div>
+            </div>
+            <div className='mobile-menu' style={showMobileMenu ? { left: 0 } : {}}>
+                <button className='icon-btn-container mobile-menu-exit-btn' onClick={handleMobileMenuToggle}>
+                    <i className="mobile-menu-times far fa-times"></i>
+                </button><br/>
+                <a className='mobile-nav-item' href='/menu'>Menu</a><br />
+                <a className='mobile-nav-item' href='/#about'>About Us</a><br />
+                <a className='mobile-nav-item' href='/#location'>Location</a><br />
+                <a className='mobile-nav-item' href='/#contact'>Contact</a><br />
             </div>
         </header>
     )
